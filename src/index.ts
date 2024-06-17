488acae73d5933cc9412952c4395bf0748b7f58b
Puskar-Roy <puskarroy600@gmail.com>
2024-06-17 13:16:27 +0530

    feat : key-value pair storage

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,52 @@
 import net from "net";
 import Parser from "redis-parser";
 
+const store: { [key: string]: string } = {};
+
 const server = net.createServer((connection) => {
   connection.on("data", (data) => {
     const parser = new Parser({
       returnReply: (reply) => {
         console.log("=> ", reply);
+        const command = reply[0].toLowerCase();
+        switch (command) {
+          case "set":
+            {
+              const key: string = reply[1];
+              const value: string = reply[2];
+              store[key] = value;
+              connection.write("+OK\r\n");
+            }
+            break;
+          case "get":
+            {
+              const key: string = reply[1];
+              const value: string = store[key];
+              if (!value) {
+                connection.write("$-1\r\n");
+              } else {
+                connection.write(`$${value.length}\r\n${value}\r\n`);
+              }
+            }
+            break;
+          default:
+            {
+              connection.write("-ERR unknown command\r\n");
+            }
+            break;
+        }
       },
       returnError(err) {
         console.log("=> ", err);
+        connection.write("-ERR internal error\r\n");
       },
     });
 
     parser.execute(data);
-    connection.write("+OK\r\n");
+  });
+
+  connection.on("error", (err) => {
+    console.error("Client Disconnectend!");
   });
 });
 
