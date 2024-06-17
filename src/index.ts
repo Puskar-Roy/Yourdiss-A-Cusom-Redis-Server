f0b7706abf1661bc5006c231c5ef89d20e6ff575
Puskar-Roy <puskarroy600@gmail.com>
2024-06-17 12:50:26 +0530

    feat : redis parser

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,20 @@
 import net from "net";
+import Parser from "redis-parser";
 
-const server = net.createServer((connection) => {});
+const server = net.createServer((connection) => {
+  connection.on("data", (data) => {
+    const parser = new Parser({
+      returnReply: (reply) => {
+        console.log("=> ", reply);
+      },
+      returnError(err) {
+        console.log("=> ", err);
+      },
+    });
+
+    parser.execute(data);
+    connection.write("+OK\r\n");
+  });
+});
 
 server.listen(6969, () => console.log("YourDiss Running On PORT 6969!"));
